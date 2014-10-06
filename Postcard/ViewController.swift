bf1c2c283cdabcd8eea6c69ed5b683e55f52d25e
Fiona <fiona@Tim-Bennnett-Odlums-iMac-2.local>
2014-10-06 19:51:40 +0100

    seccond rev

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+        // Adding a comment here to test commits
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
